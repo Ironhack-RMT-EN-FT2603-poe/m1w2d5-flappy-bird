12c033a1e4598743f44c14a1957667ec632a9c0b
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2026-04-10 15:24:53 +0200

    done

--- js/main.js
+++ js/main.js
@@ -17,6 +17,9 @@ let birdObj = null // at plash screen, the game has not started, so the bird sho
 // let tubeObj = null
 let tubesArray = []
 
+let gameIntervalId = null;
+let tubeSpawnIntervalId = null;
+
 
 //* GLOBAL GAME FUNCTIONS
 function gameStart() {
@@ -26,7 +29,7 @@ function gameStart() {
   gameScreenNode.style.display = "flex"
 
   // starting the main game interval
-  setInterval(gameLoop, Math.floor(1000/60))
+  gameIntervalId = setInterval(gameLoop, Math.floor(1000/60))
 
   // Adding the initial elements of the game
   birdObj = new Bird()
@@ -37,7 +40,7 @@ function gameStart() {
 
 
   // initialize the other intervals of the game
-  setInterval(spawnTube, 2000)
+  tubeSpawnIntervalId = setInterval(spawnTube, 2000)
 
 }
 
@@ -48,14 +51,74 @@ function gameLoop() {
   tubesArray.forEach((tubeObj) => {
     tubeObj.automaticMovement()
   })
+  tubeDespawnCheck()
+  birdTubeCollitionCheck()
 }
 
 function spawnTube() {
-  let newTube = new Tube()
-  tubesArray.push(newTube)
+
+  // random number between -200 and 0
+  const randomPositionY = Math.floor( Math.random() * -200 )
+
+  let newTubeUp = new Tube("up", randomPositionY)
+  tubesArray.push(newTubeUp)
+
+  let newTubeDown = new Tube("down", randomPositionY + 340)
+  tubesArray.push(newTubeDown)
+
   console.log(tubesArray)
 }
 
+function tubeDespawnCheck() {
+  tubesArray.forEach((tubeObj, index) => {
+    if (tubeObj.x <= 0) {
+      console.log("one tube found exiting the screen")
+
+      // When we want to remove an element from the game we need to remove it from both environments:
+      //1. DOM environment
+      tubeObj.node.remove()
+      //2 JS environment
+      tubesArray.splice(index, 1)
+
+    }
+  })
+}
+
+function birdTubeCollitionCheck() {
+
+  // birdObj
+  // tubeObj
+  tubesArray.forEach((tubeObj) => {
+    let isColliding = collisionCheck(birdObj, tubeObj)
+    if (isColliding === true) {
+      gameOver()
+    }
+  })
+
+}
+
+function collisionCheck(elem1, elem2) {
+  return (
+    elem1.x < elem2.x + elem2.width &&
+    elem1.x + elem1.width > elem2.x &&
+    elem1.y < elem2.y + elem2.height &&
+    elem1.y + elem1.height > elem2.y
+  );
+}
+
+function gameOver() {
+
+  //1. stop all intervals
+  clearInterval(gameIntervalId)
+  clearInterval(tubeSpawnIntervalId)
+
+  //2. change states
+  gameScreenNode.style.display = "none"
+  gameOverScreenNode.style.display = "flex"
+
+  //3. restart all game variables and clear the DOM
+  //! this is important to be able to restart the game
+}
 
 
 //* EVENT LISTENERS
@@ -83,11 +146,11 @@ gameBoxNode.addEventListener("click", () => {
   - create the class (x, y, width, height, speed) ✅
   - tubes will move automatically ✅
 
-- collision between the bird and the tubes
+- collision between the bird and the tubes ✅
 - spawn tubes as the game progresses ✅
-  - random y
-  - two at a time with differente images and y
-- despawn the tubes once they exit the screen
+  - random y ✅
+  - two at a time with differente images and y ✅
+- despawn the tubes once they exit the screen ✅
 
 
 BONUS
--- js/tube.js
+++ js/tube.js
@@ -1,12 +1,19 @@
 class Tube {
 
-  constructor() {
+  constructor(type, yPosition) {
+
+    this.type = type
+
     this.node = document.createElement("img")
-    this.node.src = "./images/obstacle_top.png"
+    if (this.type === "up") {
+      this.node.src = "./images/obstacle_top.png"
+    } else if (this.type === "down") {
+      this.node.src = "./images/obstacle_bottom.png"
+    }
     gameBoxNode.append(this.node) // as soon as the object is created, add the node to the game screen
 
     this.x = 550
-    this.y = 0
+    this.y = yPosition
     this.width = 50
     this.height = 250
 
